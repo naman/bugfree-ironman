2e7bdcbfe6e14fc09a2c799b8c2227a01bf720e0
Ankur Singh <ankur13019@iiitd.ac.in>
2015-03-22 05:09:59 +0530

    sync sliders

--- public/js/main.js
+++ public/js/main.js
@@ -2,5 +2,23 @@ $(document).ready(function(){
 	$('#map').vectorMap({map: 'in_mill_en'});
 	map = $("#map").vectorMap('get', 'mapObject');
 	map.setBackgroundColor("#5A708C")
-	$("#cards").slick();
+	// $("#cards").slick();		
+
+	$('#cards').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  // fade: true,
+  asNavFor: '#scroller'
+});
+$('#scroller').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '#cards',
+  // dots: true,
+  arrows: false,
+  centerMode: true,
+  focusOnSelect: true,
+  vertical: true
+});
 });
