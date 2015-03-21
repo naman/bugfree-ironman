f381dd5806581c9a70472586d0d6035fbafe7a7c
Tushar Gupta <tushar13113@iiitd.ac.in>
2015-03-22 04:42:32 +0530

    Literacy card renamed to child labor

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
 	$('#map').vectorMap({map: 'in_mill_en'});
+	map = $("#map").vectorMap('get', 'mapObject');
+	map.setBackgroundColor("#5A708C")
 	$("#cards").slick();
 });
