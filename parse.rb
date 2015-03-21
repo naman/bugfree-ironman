42a8eccf428e6b26dfb27597b7ea3b343e7e957a
Ankur Singh <ankur13019@iiitd.ac.in>
2015-03-22 04:39:43 +0530

    added new fields and their api

--- parse.rb
+++ parse.rb
@@ -2,26 +2,106 @@ require 'sinatra'
 
 require 'spreadsheet'
 require 'json'
-
+set :static_cache_control, [:public, {:max_age => 300}]
 get '/api' do
 	content_type :json
 	Spreadsheet.client_encoding = 'UTF-8'
 	book = Spreadsheet.open 'state.xls'
 	sheet1 = book.worksheet 0
 	state = {}
-	# 0-rank 1-name 2-type 3-population 4-% 5-males 6-females 7-literacy% 8-rural 9-urban 10-area-km^2 11-desity
-	# male,female,literate,illiterate,rural,urban,km2/people
+	# 0-name 1-population 2-% 3-males 4-females 5-literacy% 6-rural 7-urban 8-area-km^2 9-desity
+	# 10-child labour %,11- utilization of govt health services,12- child marriage girls, 13- child marriage boys,
+	# %age - 14 - total toilet 15 - rural toilet 16 - urban toilet 
+	# 17 unemployment rural /100, 18- unemployment urban/100
+	# male,female,literate,illiterate,rural,urban, child_labour, health_services
+	row0 = sheet1.row(0)
+	cmg = []
+	cmb = []
+	hs = []
+	ru = []
+	uu = []
+	childl_def = row0[10]
 	sheet1.each do |row|
-		# puts row[5]
-		# puts row[6]
-		# puts row[3]
-		male = (row[5].to_f/row[3].to_f*100.0).round
+		male = (row[3].to_f/row[1].to_f*100.0).round
 		female = 100 - male
-		l = (row[7]).round
+		l = (row[5]).round
 		ul = 100 - l
-		rural = (row[8].to_f/row[3].to_f*100.0).round
+		rural = (row[6].to_f/row[1].to_f*100.0).round
 		urban = 100 -rural
-	  state[row[1]] = {male: male,female: female,literate: l,illiterate: ul,urban: urban,rural: rural}
+
+		#stage 2
+		#child labour calcs
+		if row[10].nil?
+			childl = childl_def
+		else
+			childl = row[10]
+		end
+		if childl <= 1.7
+			childl = 1
+		elsif childl <= 2.6
+			childl = 2
+		else
+			childl = 3
+		end
+
+		total_toilet = row[14].round
+		urban_toilet = row[16].round
+		rural_toilet = row[15].round
+
+		state[row[0]] = {male: male,female: female,literate: l,illiterate: ul,urban: urban,rural: rural,child_labour: childl,total_toilet: total_toilet,urban_toilet: urban_toilet,rural_toilet: rural_toilet}
+
+		#accumulate next three parameters
+		if row[11]
+			heal = row[11]
+			hs << heal
+			state[row[0]][:health] = heal
+		end
+		if row[12]
+			cgirl = row[12]
+			cmg << cgirl
+			state[row[0]][:child_marriage_girl] = (cgirl*0.15).ceil
+		end
+		if row[13]
+			cboy = row[13]
+			cmb << cboy
+			state[row[0]][:child_marriage_boy] = (cboy*0.15).ceil
+		end
+
+		#employment
+		if row[17]
+			rural_unemployed = row[17]
+			ru << rural_unemployed
+			state[row[0]][:rural_unemployed] = rural_unemployed
+		end
+		if row[18]
+			urban_unemployed = row[18]
+			uu << urban_unemployed
+			state[row[0]][:urban_unemployed] = urban_unemployed
+		end
+	end
+	state[row0[0]][:rural_unemployed] = ru.sum/ru.size.to_f.round
+	state[row0[0]][:urban_unemployed] = uu.sum/uu.size.to_f.round
+	state[row0[0]][:health] = hs.sum/hs.size.to_f.round
+	state[row0[0]][:child_marriage_boy] = (cmb.sum*0.15/cmb.size.to_f).ceil
+	state[row0[0]][:child_marriage_girl] = (cmg.sum*0.15/cmg.size.to_f).ceil
+	sheet1.each do |row|
+
+		#Time to refill those parameters for empty with average
+		if not row[11]
+			state[row[0]][:health] = state[row0[0]][:health]
+		end
+		if not row[12]
+			state[row[0]][:child_marriage_girl] = state[row0[0]][:child_marriage_girl]
+		end
+		if not row[13]
+			state[row[0]][:child_marriage_boy] =  state[row0[0]][:child_marriage_boy]
+		end
+		if not row[17]
+			state[row[0]][:rural_unemployed] = state[row0[0]][:rural_unemployed]
+		end
+		if not row[18]
+			state[row[0]][:urban_unemployed] =  state[row0[0]][:urban_unemployed]
+		end
 	end
 	state.to_json
 end
